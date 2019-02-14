9b23703e112d42cbc84014083fb05bf4d7a22a11
Dan Shrader <Dan@Shraderland.com>
2019-02-13 20:48:36 -0500

    pathing...........

--- startScript.sh
+++ startScript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Simple script to start ET Legacy client/listen server with Omni-Bots
 #
-cd /root/etlegacy/
-/root/etlegacy/etl +set omnibot_enable 1 +set omnibot_path "/root/etlegacy/legacy/omni-bot"
+echo cd /root/etlegacy/
+./etl +set omnibot_enable 1 +set omnibot_path "/root/etlegacy/legacy/omni-bot"
